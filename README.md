8d5732221609180e627e2943a66adbc22232ab00
Girma Nigusse <xgirma@gmail.com>
2020-01-21 21:39:55 -0500

    set CI

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Puppeteer: (commonjs, mocha, chai)
 
+[![GitHub Actions status | e2e-boilerplates/puppeteer-commonjs-mocha-chai](https://github.com/e2e-boilerplates/puppeteer-commonjs-mocha-chai/workflows/NodeCI/badge.svg)](https://github.com/e2e-boilerplates/puppeteer-commonjs-mocha-chai/actions?workflow=NodeCI)
+
 Puppeteer end-to-end test automation boilerplate project. Using commonjs, mocha and chai.
 
 ## Getting Started
--- spec/search.spec.js
+++ spec/search.spec.js
@@ -1,22 +1,22 @@
-/* eslint-disable no-unused-expressions */
 const puppeteer = require("puppeteer");
 const { expect } = require("chai");
 
 let page;
 let browser;
-const searchBox = ".gLFyf.gsfi";
 
-describe("google search", () => {
+describe("Sandbox", () => {
   before(async () => {
-    browser = await puppeteer.launch({ headless: false });
+    browser = process.env.GITHUB_ACTIONS
+      ? await puppeteer.launch()
+      : await puppeteer.launch({ headless: false });
+
     page = await browser.newPage();
 
-    await Promise.race([
-      page
-        .goto("https://www.google.com", { waitUntil: "networkidle0" })
-        .catch(() => {}),
-      page.waitFor("body", { timeout: 6000 }).catch(() => {})
-    ]);
+    await page
+      .goto("https://xgirma.github.io/sandbox/", {
+        waitUntil: "networkidle0"
+      })
+      .catch(() => {});
   });
 
   after(() => {
@@ -25,25 +25,11 @@ describe("google search", () => {
     }
   });
 
-  it("should be on google search page", async () => {
-    await page.waitFor(searchBox);
+  it("should be on the sandbox", async () => {
+    await page.waitFor("h1");
+    const title = await page.$eval("h1", el => el.textContent);
 
-    const title = await page.title();
-    expect(title).to.equal("Google");
-  });
-
-  it("should search for Cheese!", async () => {
-    expect(!!(await page.$(searchBox))).to.be.true;
-
-    await page.type(searchBox, "Cheese!", { delay: 100 });
-    await page.keyboard.press("\n");
-  });
-
-  it('the page title should start with "Cheese!', async () => {
-    await page.waitFor("#resultStats");
-
-    const title = await page.title();
-    const words = title.split(" ");
-    expect(words[0]).to.equal("Cheese!");
+    expect(await page.title()).to.equal("Sandbox");
+    expect(title).to.equal("Sandbox");
   });
 });
